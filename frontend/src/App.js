234351405cf222cfd27598c8336aa03245251a79
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2025-01-28 21:34:10 +0530

    better-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,25 +6,25 @@ function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", paddingTop: "50px", height: "100vh" }}>
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", paddingTop: "50px", height: "90vh" }}>
       <Navbar />
-      <div style={{ border: "2px solid #007bff", padding: "20px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", borderRadius: "10px" }}>
-      <h1 style={{color: "#007bff"}}>Welcome to StreakHub</h1>
+      <div style={{ backgroundColor: "#007bff", border: "2px solid #007bff", padding: "20px", boxShadow: "0 2px 10px rgba(0, 99, 198, 0.85)", borderRadius: "15px", maxWidth: "250px" }}>
+      <h1 style={{color: "white"}}>Welcome to StreakHub</h1>
       <button
         onClick={() => setIsModalOpen(true)}
         style={{
-          background: "#007bff",
-          color: "white",
+          background: "white",
+          color: "#007bff",
           padding: "10px 20px",
           margin: "10px",
           fontSize: "16px",
           border: "none",
-          borderRadius: "5px",
+          borderRadius: "10px",
           cursor: "pointer",
           transition: "0.2s ease-in-out",
         }}
-        onMouseOver={(e) => (e.target.style.background = "#0056b3")}
-        onMouseOut={(e) => (e.target.style.background = "#007bff")}
+        onMouseOver={(e) => (e.target.style.background = "white")}
+        onMouseOut={(e) => (e.target.style.background = "white")}
       >Learn More</button>
       </div>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title="Hello, World!">
