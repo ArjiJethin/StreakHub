23864538d5490f7657b7b28b3b9677309dd4084e
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2025-01-29 08:56:23 +0530

    whatever!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import Modal from "./Components/Modal"; 
-import Navbar from "./Components/Navbar";
+import Modal from "./Components/Modal/Modal"; 
+import Navbar from "./Components/Navbar/Navbar";
+import Button from "./Components/Button/Button";
 import "./App.css";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="home">
-      <Navbar />
+      <div className="center">
+        <Navbar className="navBar" />
+      </div>
       <div className="box">
         <h1>Welcome to StreakHub</h1>
         <button className="button" onClick={() => setIsModalOpen(true)}>Learn More</button>
+        <Button onClick={() => setIsModalOpen(true)}>Learn More</Button>
       </div>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title="Nuh uh">
         <p>This project is underdevelopment stay tuned!!</p>
